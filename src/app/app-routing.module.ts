481c0dccf7189d1583b2be7999e09835cc2d47e1
fastenakelsk <kilian.fastenakels@gmail.com>
2019-11-14 16:02:46 +0100

    Basic Setup

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,22 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
+import { RegisterComponent } from './components/register/register.component';
+import { LoginComponent } from './components/login/login.component';
+import { FriendsListComponent } from './components/friends-list/friends-list.component';
+import { PollComponent } from './components/poll/poll.component';
 
-const routes: Routes = [];
+const routes:Routes = [
+  { path: 'register', component: RegisterComponent },
+  { path: 'login', component: LoginComponent },
+  { path: 'friendslist', component: FriendsListComponent },
+  { path: 'newpoll', component: PollComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {
+  
+ }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,13 +4,28 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
+import { RouterModule, Routes } from '@angular/router';
+import { RegisterComponent } from './components/register/register.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { LoginComponent } from './components/login/login.component';
+import { FriendsListComponent } from './components/friends-list/friends-list.component';
+import { PollComponent } from './components/poll/poll.component';
+import { PollItemComponent } from './components/poll-item/poll-item.component';
+
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    RegisterComponent,
+    NavbarComponent,
+    LoginComponent,
+    FriendsListComponent,
+    PollComponent,
+    PollItemComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    RouterModule
   ],
   providers: [],
   bootstrap: [AppComponent]
